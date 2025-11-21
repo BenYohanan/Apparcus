9ad258b846dbb21a673c80e8b4361783367f02d2
SirRaph <raphaelezeomas@gmail.com>
2025-11-21 13:03:33 +0100

    User's Dashboard

--- Apparcus/Controllers/UserController.cs
+++ Apparcus/Controllers/UserController.cs
@@ -1,8 +1,11 @@
-﻿using Core.Models;
+﻿using Core.DbContext;
+using Core.Models;
 using Core.ViewModels;
-using Logic.Helpers;
 using Logic.IHelpers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Text.Json;
 
 namespace Apparcus.Controllers
@@ -11,14 +14,59 @@ namespace Apparcus.Controllers
     {
         private readonly IUserHelper _userHelper;
         private readonly IProjectHelper _projectHelper;
+        private readonly AppDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UserController(IUserHelper userHelper, IProjectHelper projectHelper)
+        public UserController(IUserHelper userHelper, IProjectHelper projectHelper, AppDbContext context, UserManager<ApplicationUser> userManager)
         {
             _userHelper = userHelper;
             _projectHelper = projectHelper;
+            _context = context;
+            _userManager = userManager;
         }
-
         public async Task<IActionResult> Index()
+        {
+            ViewBag.Layout = _userHelper.GetRoleLayout();
+
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Account");
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null) return NotFound();
+
+            var userVm = new ApplicationUserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                FullName = $"{user.FirstName} {user.LastName}".Trim(),
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                DateOfBirth = user.DateOfBirth,
+                DateRegistered = user.DateCreated,
+              
+            };
+            var numberOfProject = await _context.Projects
+                 .Where(p => p.CreatedById == userId && !p.Deleted)
+                 .CountAsync();
+            var supporterRows = await _context.ProjectSupporters
+                .Where(s => s.Email == user.Email && !s.Deleted)
+                .ToListAsync();
+            var validSupporters = supporterRows
+                .Where(s => decimal.TryParse(s.Amount, out _))
+                .ToList();
+            ViewBag.TotalProjects = numberOfProject;
+
+            ViewBag.TotalSupported = validSupporters.Count;
+
+            ViewBag.TotalRaised = validSupporters
+                .Sum(s => decimal.Parse(s.Amount ?? "0"));
+
+            return View(userVm);
+        }
+        public async Task<IActionResult> MyProject()
         {
             ViewBag.Layout = _userHelper.GetRoleLayout();
             var userId = _userHelper.GetCurrentUserId();
